4b769e5d07de585294bedf7bfb7e67cb4faf834d
Carlos Eduardo Ferreira <carlosqlz.cef@gmail.com>
2022-05-20 00:23:33 -0300

    i.py feito depois de dias!!!!!!!

--- 1/Projeto de Desenvolvimento de Algoritmos/N2/Exercícios/While/i.py
+++ 1/Projeto de Desenvolvimento de Algoritmos/N2/Exercícios/While/i.py
@@ -1,11 +1,19 @@
-atual = 0
-anterior = 0
-proximo = 0
 cont = 0
-while cont <=15:
-    proximo = atual + anterior
-    print(proximo)
-    atual = cont + anterior
-    anterior = atual - cont
-    cont = cont +1
-    
+proximo = 0
+while (cont >= 0) and (cont <= 14):
+    if (cont == 0):
+        atual = 0
+        print(atual)
+        cont = cont + 1
+    elif (cont == 1):
+        anterior = 0
+        atual = 1
+        print(atual)
+        cont = cont + 1
+    while (cont >= 2) and (cont <= 14):
+        proximo = atual + anterior
+        print(proximo)
+        anterior = atual
+        atual = proximo
+        cont = cont + 1
+        
