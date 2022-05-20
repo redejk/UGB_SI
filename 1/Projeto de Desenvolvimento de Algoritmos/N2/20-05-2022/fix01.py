1b802d066c8832140c2b75f662cfe00bb64e3b39
Carlos Eduardo Ferreira <carlosqlz.cef@gmail.com>
2022-05-20 03:58:29 -0300

    for

---
+++ 1/Projeto de Desenvolvimento de Algoritmos/N2/20-05-2022/fix01.py
@@ -0,0 +1,4 @@
+fatorial = 1
+for i in range(1,6):
+    fatorial = fatorial * i
+print(fatorial)
---
+++ 1/Projeto de Desenvolvimento de Algoritmos/N2/Exercícios/For/3.py
@@ -0,0 +1,13 @@
+base = 3
+resultado = 0
+for i in range(0, 16):
+    if (i == 0):
+        resultado = 1
+        print(resultado)
+    elif (i == 1):
+        resultado = base
+        print(resultado)
+    else:
+        (i >= 2) and (i <=16)
+        resultado = resultado * base
+        print(resultado)
