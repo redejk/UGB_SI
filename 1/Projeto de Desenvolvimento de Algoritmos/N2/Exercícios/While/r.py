af3d997356994fb4497f72e824be082e57cc0d53
Carlos Eduardo Ferreira <carlosqlz.cef@gmail.com>
2022-05-20 03:22:11 -0300

    r

--- 1/Projeto de Desenvolvimento de Algoritmos/N2/Exercícios/While/r.py
+++ 1/Projeto de Desenvolvimento de Algoritmos/N2/Exercícios/While/r.py
@@ -1,12 +1,11 @@
 numero = 0
 maior = 0
 menor = 0
-while (numero >= 0):
+while (numero >=0):
     numero = int(input("Digite um valor: "))
-    if (numero >= maior):
-        maior = numero
-    if (numero <= menor):
-        menor = numero
-    print(maior)
-    print(menor)
+    if (numero > 0):
+        
 print(f"O maior valor digitado é: {maior} e o menor valor é : {menor}")
+
+
+
