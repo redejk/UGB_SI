342b4e72bfc3601905c158e38e8c02c26d7bebc1
Carlos Eduardo Ferreira <carlosqlz.cef@gmail.com>
2022-09-06 01:17:36 -0300

    Update ex05.py

--- 2/Programação I/Aula 5/ex05.py
+++ 2/Programação I/Aula 5/ex05.py
@@ -8,11 +8,10 @@ while True:
         nt1 = float(input("Digite sua nota 1: "))
         nt2 = float(input("Digite sua nota 2: "))
         total.append((nome, nt1, nt2))
-        print(total)
 
 for i in total:
-    nome = total[i][0]
-    media = (total[i][1] + total[i][2])/2
+    nome = i[0]
+    media = (i[1] + i[2])/2
     if media >= 7.0:
         print(f'{nome} ficou com media: {media} e foi aprovado\n')
     else:
