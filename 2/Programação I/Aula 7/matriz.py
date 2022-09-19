870f48d4e799e8ec66bacfe3b559010aa2184a8b
Carlos Eduardo Ferreira <carlosqlz.cef@gmail.com>
2022-09-19 20:22:49 -0300

    aula 7

---
+++ 2/Programação I/Aula 7/matriz.py
@@ -0,0 +1,12 @@
+matriz = [
+    [1, 'A', 33],
+    [2, 'B', 22],
+    [3, 'C', 11],
+    [4, 'D', 99]
+]
+
+# for linha in matriz:
+#     print(linha)
+
+for linha in range(len(matriz)):
+    print(matriz[linha])
---
+++ 2/Programação I/Aula 7/matriz_linha_e_coluna.py
@@ -0,0 +1,31 @@
+notas = [
+    [3,5,6],
+    [4,2,9],
+    [9,7,5],
+    [1,1,1]
+]
+
+# soma = 0
+# for nota in notas:
+#     somalinha = 0
+#     for notinha in nota:
+#         somalinha = somalinha + notinha
+#         soma = soma + notinha
+#     print(somalinha)
+# print(soma)
+
+
+# matriz2x2 = [
+#     [1,2],
+#     [3,4]
+# ]
+# for i in matriz2x2:
+#     print(i)
+
+#     for j in i:
+#         print(j)
+
+# soma = 0
+# for i in range(len(notas)):
+#     for j in range(len(notas[i])):
+#         print(notas[i][j])
