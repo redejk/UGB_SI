8d46379f71bff9f90720fc00b502aaa9d3498673
Carlos Eduardo Ferreira <carlosqlz.cef@gmail.com>
2022-11-01 15:44:04 -0300

    Update 3.py

--- 2/Programação I/N2/Aula 8/exercicios/3.py
+++ 2/Programação I/N2/Aula 8/exercicios/3.py
@@ -18,5 +18,9 @@ class Retangulo:
     
 ladoA = float(input("Digite o valor do lado A: "))
 ladoB = float(input("Digite o valor do lado B: "))
+tamanho_piso = float(input("Digite o tamanho do piso: "))
 retangulo = Retangulo(ladoA, ladoB)
+area = retangulo.calcular_area()
+quantidade_pisos = (area) / (tamanho_piso)
 print(f'A area é: {retangulo.calcular_area()}\nO perimetro é: {retangulo.calcular_perimetro()}')
+print(f'Será necessário utilizar {round(quantidade_pisos)} pisos para completar a área')
