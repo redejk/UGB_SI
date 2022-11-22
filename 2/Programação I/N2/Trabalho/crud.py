dff4f447c86a2c3a97e95952c1f268f804df2d88
Carlos Eduardo Ferreira <carlosqlz.cef@gmail.com>
2022-11-22 12:47:21 -0300

    aula

---
+++ 2/Programação I/N2/Trabalho/crud.py
@@ -0,0 +1,61 @@
+class MyCrud:
+    def __init__(self):
+        import sqlite3
+        self.conexao = sqlite3.connect('moleza.sqlite')
+        self.cursor = self.conexao.cursor()
+    
+    def fecharDB(self):
+        self.conexao.close()
+
+    def criarTabela(self):
+        sql = """
+            create table if not exists pessoas(
+                id integer not null primary key autoincrement,
+                nome text not null,
+                cpf text caracter (11)
+            );
+        """
+        self.cursor.execute(sql)
+        print('Tabela criada...')
+
+    def selecionar(self):
+        sql = """
+            select * from pessoas;
+        """
+        resultado = self.cursor.execute(sql)
+        resultado = self.cursor.fetchall()
+        for i in resultado:
+            print(i)
+
+    def inserir (self, nome, cpf):
+        self.nome = nome
+        self.cpf = cpf
+        sql = f"""
+            insert into pessoas (nome, cpf)
+            values('{nome}', '{cpf}');
+        """
+        self.cursor.execute(sql)
+        print('Salvo com sucesso...')
+
+    def alterar(self, id, nome, cpf):
+        self.id = id
+        self.nome = nome
+        self.cpf = cpf
+        sql = f"""
+            update pessoas
+            set nome = '{self.nome}', cpf = '{self.cpf}'
+            where id = {self.id};
+        """
+        self.cursor.execute(sql)
+        self.conexao.commit()
+        print('alterado com sucesso...')
+
+    def deletar(self, id):
+        self.id = id
+        sql = f"""
+            delete from pessoas
+            where id = {self.id};
+        """
+        self.cursor.execute(sql)
+        self.conexao.commit()
+        print('deletado com sucesso...')
---
+++ 2/Programação I/N2/Trabalho/main.py
@@ -0,0 +1,41 @@
+from crud import MyCrud
+crud = MyCrud()
+
+crud.criarTabela()
+
+while True:
+    print("""
+    Escolha uma opção:
+    1 - Incluir
+    2 - Ler
+    3 - Alterar
+    4 - Deletar
+    5 - Sair
+    """)
+    valor = input('-->')
+    if valor == '1':
+        nome = input('Digite um nome: ')
+        while True:
+            cpf = input('Digite o cpf: ')
+            if len(cpf) != 12:
+                print('O cpf deve ter 11 digitos e ser digitado sem ponto ou hífen')
+            else:
+                break
+        crud.inserir(nome, cpf)
+    elif valor == '2':
+        crud.selecionar()
+    elif valor == '3':
+        id = int(input("Digite o ID que deseja alterar: "))
+        nome = input('Digite o nome: ')
+        while True:
+            cpf = input('Digite o cpf: ')
+            if len(cpf) != 11:
+                print('O cpf deve ter 11 digitos e ser digitado sem ponto ou hífen')
+            else:
+                break
+        crud.alterar(id, nome, cpf)
+    elif valor =='4':
+        id = int(input('Digite o ID que deseja deletar: '))
+        crud.deletar(id)
+    else:
+        break
