705ebbdbe83fbcf7a06e280841bd223818d638f2
Carlos Eduardo Ferreira <carlosqlz.cef@gmail.com>
2022-12-03 20:11:47 -0300

    Update 1.py

--- 2/Programação I/N2/Aula 8/exercicios/1.py
+++ 2/Programação I/N2/Aula 8/exercicios/1.py
@@ -1,17 +1,17 @@
 class Bola:
     def __init__(self, cor, circunferencia, material):
-        self.__cor = cor
+        self.cor = cor
         self.circunferencia = circunferencia
         self.material = material
 
     def trocaCor(self, cor):
-        self.__cor = cor
+        self.cor = cor
         
     def mostraCor(self):
-        print(self.__cor)
+        print(self.cor)
 
     def retornarCor(self):
-        return self.__cor
+        return self.cor
 
 bola = Bola('Azul', 5, 'couro')
 bola.mostraCor()
