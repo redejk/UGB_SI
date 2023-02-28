d145707ee37d4a5739eeb59ea5bab0d41ad285b6
Carlos Eduardo Ferreira <carlosqlz.cef@gmail.com>
2023-02-27 21:43:18 -0300

    .

--- 3/Programação 2/N1/projeto_django/mysite/views.py
+++ 3/Programação 2/N1/projeto_django/mysite/views.py
@@ -1,3 +1,15 @@
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse('UGB')
+    html = """
+    <html>
+    <head>
+        <title>Aula 3</title>
+    </head>
+    <body>
+        <h1>Carlos</h1>
+    </body>
+
+    </html>
+
+    """
+    return HttpResponse(html)
