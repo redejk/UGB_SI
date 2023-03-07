7b14cc7498f06f5fca2eba41849b56b0ce55e3d3
Carlos Eduardo Ferreira <carlosqlz.cef@gmail.com>
2023-03-06 21:33:56 -0300

    .

--- 3/Programação 2/N1/projeto_django/mysite/mysite/urls.py
+++ 3/Programação 2/N1/projeto_django/mysite/mysite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from views import index
+from core.views import index
 
 urlpatterns = [
     path('index/', index),
