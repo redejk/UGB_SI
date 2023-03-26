20e111f9e9570a9927d1ca4bfc925e7daefede51
Carlos Eduardo Ferreira <carlosqlz.cef@gmail.com>
2023-03-26 20:14:25 -0300

    .

--- 3/Estrutura de Dados/N1/Trabalho/3_QuickSort.py
+++ 3/Estrutura de Dados/N1/Trabalho/3_QuickSort.py
@@ -1,4 +1,5 @@
 import time
+import random
 
 def quicksort(vet, inicio=0, fim=None):
     fim = fim if fim != None else len(vet)-1
@@ -20,6 +21,17 @@ def particao(vet, inicio, fim):
 #a)Ordenado 5000
 vet = []
 for i in range(1,5001):
+    vet.append(i)
+inicio = time.time()
+quicksort(vet)
+fim = time.time()
+print(vet)
+print(fim - inicio)
+print(len(vet))
+
+# #a)Ordenado 10000
+vet = []
+for i in range(1, 10001):
   vet.append(i)
 inicio = time.time()
 quicksort(vet)
@@ -27,68 +39,58 @@ fim = time.time()
 print(vet)
 print(fim - inicio)
 
-# #a)Ordenado 10000
-# vet = []
-# for i in range(1, 10001):
-#   vet.append(i)
-# inicio = time.time()
-# mergeSort(vet)
-# fim = time.time()
-# print(vet)
-# print(fim - inicio)
-
 # #b)Ordem inversa 5000
-# vet = []
-# for i in range(5000, 0, -1):
-#   vet.append(i)
-# inicio = time.time()
-# mergeSort(vet)
-# fim = time.time()
-# print(vet)
-# print(fim - inicio)
+vet = []
+for i in range(5000, 0, -1):
+  vet.append(i)
+inicio = time.time()
+quicksort(vet)
+fim = time.time()
+print(vet)
+print(fim - inicio)
 
 # #b)Ordem inversa 10000
-# vet = []
-# for i in range(10000, 0, -1):
-#   vet.append(i)
-# inicio = time.time()
-# mergeSort(vet)
-# fim = time.time()
-# print(vet)
-# print(fim - inicio)
+vet = []
+for i in range(10000, 0, -1):
+  vet.append(i)
+inicio = time.time()
+quicksort(vet)
+fim = time.time()
+print(vet)
+print(fim - inicio)
 
-# #c)Ordem aleatoria 1 5000
-# vet = []
-# vet = random.sample(range(1, 15000), 5000)
-# inicio = time.time()
-# mergeSort(vet)
-# fim = time.time()
-# print(vet)
-# print(fim - inicio)
+#c)Ordem aleatoria 1 5000
+vet = []
+vet = random.sample(range(1, 15000), 5000)
+inicio = time.time()
+quicksort(vet)
+fim = time.time()
+print(vet)
+print(fim - inicio)
 
-# #c)Ordem aleatoria 1 10000
-# vet = []
-# vet = random.sample(range(1, 15000), 10000)
-# inicio = time.time()
-# mergeSort(vet)
-# fim = time.time()
-# print(vet)
-# print(fim - inicio)
+#c)Ordem aleatoria 1 10000
+vet = []
+vet = random.sample(range(1, 15000), 10000)
+inicio = time.time()
+quicksort(vet)
+fim = time.time()
+print(vet)
+print(fim - inicio)
 
-# #d)Ordem aleatoria 2 5000
-# vet = []
-# vet = random.sample(range(3000, 15000), 5000)
-# inicio = time.time()
-# mergeSort(vet)
-# fim = time.time()
-# print(vet)
-# print(fim - inicio)
+#d)Ordem aleatoria 2 5000
+vet = []
+vet = random.sample(range(3000, 15000), 5000)
+inicio = time.time()
+quicksort(vet)
+fim = time.time()
+print(vet)
+print(fim - inicio)
 
-# #d)Ordem aleatoria 2 10000
-# vet = []
-# vet = random.sample(range(3000, 15000), 10000)
-# inicio = time.time()
-# mergeSort(vet)
-# fim = time.time()
-# print(vet)
-# print(fim - inicio)
+#d)Ordem aleatoria 2 10000
+vet = []
+vet = random.sample(range(3000, 15000), 10000)
+inicio = time.time()
+quicksort(vet)
+fim = time.time()
+print(vet)
+print(fim - inicio)
---
+++ 3/Estrutura de Dados/N1/Trabalho/3_QuickSortAlterado.py
@@ -0,0 +1,96 @@
+import time
+import random
+
+def quicksort(vet, inicio=0, fim=None):
+    fim = fim if fim != None else len(vet)-1
+    if inicio < fim:
+        pivo = insertionSort(vet, inicio, fim)
+        quicksort(vet, inicio, pivo-1)
+        quicksort(vet, pivo+1, fim)
+
+def insertionSort(vet, inicio, fim):
+    for i in range(inicio+1, fim+1):
+        atual = vet[i]
+        j = i - 1
+        while j >= 0 and atual < vet[j]:
+            vet[j+1] = vet[j]
+            j = j - 1
+        vet[j+1] = atual
+    return j + 1
+
+#a)Ordenado 5000
+# vet = []
+# for i in range(1,5001):
+#     vet.append(i)
+# inicio = time.time()
+# quicksort(vet)
+# fim = time.time()
+# print(vet)
+# print(fim - inicio)
+# print(len(vet))
+
+# #a)Ordenado 10000
+# vet = []
+# for i in range(1, 10001):
+#   vet.append(i)
+# inicio = time.time()
+# quicksort(vet)
+# fim = time.time()
+# print(vet)
+# print(fim - inicio)
+
+# #b)Ordem inversa 5000
+# vet = []
+# for i in range(5000, 0, -1):
+#   vet.append(i)
+# inicio = time.time()
+# quicksort(vet)
+# fim = time.time()
+# print(vet)
+# print(fim - inicio)
+
+# #b)Ordem inversa 10000
+# vet = []
+# for i in range(10000, 0, -1):
+#   vet.append(i)
+# inicio = time.time()
+# mergeSort(vet)
+# fim = time.time()
+# print(vet)
+# print(fim - inicio)
+
+#c)Ordem aleatoria 1 5000
+vet = []
+vet = random.sample(range(1, 15000), 5000)
+inicio = time.time()
+quicksort(vet)
+fim = time.time()
+print(vet)
+print(fim - inicio)
+
+# #c)Ordem aleatoria 1 10000
+# vet = []
+# vet = random.sample(range(1, 15000), 10000)
+# inicio = time.time()
+# quicksort(vet)
+# fim = time.time()
+# print(vet)
+# print(fim - inicio)
+
+# #d)Ordem aleatoria 2 5000
+# vet = []
+# vet = random.sample(range(3000, 15000), 5000)
+# inicio = time.time()
+# quicksort(vet)
+# fim = time.time()
+# print(vet)
+# print(fim - inicio)
+
+# #d)Ordem aleatoria 2 10000
+# vet = []
+# vet = random.sample(range(3000, 15000), 10000)
+# inicio = time.time()
+# quicksort(vet)
+# fim = time.time()
+# print(vet)
+# print(fim - inicio)
